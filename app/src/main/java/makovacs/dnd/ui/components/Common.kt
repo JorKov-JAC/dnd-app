bbb1bb5c81e43c9911c98fe64f95145e15e8e78e
Jordan Kovacs <98484926+JorKov-JAC@users.noreply.github.com>
2023-11-08 14:03:28 -0500

    feat: Implement EditableList component

--- app/src/main/java/makovacs/dnd/ui/components/Common.kt
+++ app/src/main/java/makovacs/dnd/ui/components/Common.kt
@@ -2,7 +2,12 @@ package makovacs.dnd.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -10,16 +15,28 @@ import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -150,6 +167,205 @@ fun <T> SelectableList(
                         .padding(8.dp)
                 ) {
                     itemContent(it)
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Displays a list and allows the user to modify it.
+ *
+ * @param items The list of items to display.
+ * @param onSwap Called when the user swaps two items.
+ * @param key Converts an item to a unique key.
+ * This is necessary if [itemContent] needs to persist state when [items] is modified.
+ * @param selectedIndexState A state representing the currently selected item, or null if there is
+ * no selected item.
+ * @param onCreateDialogContent Component for creating a new item,
+ * or null if the user cannot create items.
+ * Is given the index at which the new item should be inserted.
+ * Should call onDone when an item has finished being added.
+ * @param onDelete Called when the user tries to delete an item with the given index,
+ * or null if the user cannot delete items.
+ * @param itemToName Gets an item's user-visible name.
+ * @param onEditDialogContent Component for editing an item,
+ * or null if the user cannot edit items.
+ * Is given the item and its index.
+ * Should call onDone when an item has finished being edited.
+ * @param title Optional title to display.
+ * @param itemContent Converts an item into a component.
+ */
+@Composable
+fun <T> EditableList(
+    items: List<T>,
+    onSwap: (from: Int, to: Int) -> Unit,
+    modifier: Modifier = Modifier,
+    key: ((T)->Any)? = null,
+    // The only way for callers to *optionally* interact with the selected index is to expose a
+    // state for it. Having a value and a setter instead would force the caller to provide them:
+    selectedIndexState: MutableState<Int?> = rememberSaveable {
+        mutableStateOf(
+            null
+        )
+    },
+    onCreateDialogContent: (@Composable (insertionIndex: Int, onDone: ()->Unit)->Unit)? = null,
+    onDelete: ((Int)->Unit)? = null,
+    itemToName: ((T)->String)? = null,
+    onEditDialogContent: (@Composable (itemIndex: Int, item: T, onDone: ()->Unit) -> Unit)? = null,
+    title: String? = null,
+    itemContent: @Composable (T)->Unit,
+) {
+    var selectedIndex by selectedIndexState
+
+    Column(modifier = modifier) {
+        // Header
+        // TODO This condition is fragile and may break if we forget to update
+        //      it when adding new header items, replace with something cleaner:
+        if (title != null || onCreateDialogContent != null) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .background(MaterialTheme.colorScheme.primary)
+                    .padding(8.dp)
+            ) {
+
+                // Title
+                if (title != null) {
+                    Text(
+                        title,
+                        style = MaterialTheme.typography.titleLarge,
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                }
+
+                Spacer(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth()
+                )
+
+                // "Create" button
+                if (onCreateDialogContent != null) {
+                    var createDialogEnabled by rememberSaveable {
+                        mutableStateOf(
+                            false
+                        )
+                    }
+                    IconButton(onClick = { createDialogEnabled = true }) {
+                        Icon(
+                            Icons.Default.Add,
+                            stringResource(R.string.create),
+                            tint = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
+
+                    if (createDialogEnabled) CardDialog(
+                        onDismissRequest = { createDialogEnabled = false }) {
+                        val insertionIndex =
+                            selectedIndex?.let { it + 1 } ?: items.size
+
+                        onCreateDialogContent(insertionIndex) {
+                            createDialogEnabled = false
+                            selectedIndex = insertionIndex
+                        }
+                    }
+                }
+            }
+        }
+
+        // Display items
+        SelectableList(
+            items = items,
+            key = key,
+            selectedIndex = selectedIndex,
+            setSelectedIndex = { selectedIndex = it },
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxSize(),
+            itemContent = itemContent
+        )
+
+        // Mutation buttons
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.tertiaryContainer)
+                .fillMaxWidth()
+        ) {
+            // Up button
+            IconButton(
+                enabled = selectedIndex?.let { it > 0 } ?: false,
+                onClick = {
+                    onSwap(selectedIndex!!, selectedIndex!! - 1)
+                    selectedIndex = selectedIndex!! - 1
+                }
+            ) {
+                Icon(
+                    Icons.Default.KeyboardArrowUp,
+                    stringResource(R.string.move_up),
+                )
+            }
+
+            // Down button
+            IconButton(
+                enabled = selectedIndex?.let { it < items.size - 1 } ?: false,
+                onClick = {
+                    onSwap(selectedIndex!!, selectedIndex!! + 1)
+                    selectedIndex = selectedIndex!! + 1
+                }
+            ) {
+                Icon(
+                    Icons.Default.KeyboardArrowDown,
+                    stringResource(R.string.move_down),
+                )
+            }
+
+            // Edit button
+            if (onEditDialogContent != null) {
+                var editDialogEnabled by rememberSaveable { mutableStateOf(false) }
+                IconButton(
+                    enabled = selectedIndex != null,
+                    onClick = { editDialogEnabled = true }
+                ) {
+                    Icon(
+                        Icons.Default.Edit,
+                        stringResource(R.string.edit),
+                    )
+                }
+
+                if (editDialogEnabled) CardDialog(
+                    onDismissRequest = { editDialogEnabled = false }) {
+                    onEditDialogContent(
+                        selectedIndex!!,
+                        items[selectedIndex!!]
+                    ) { editDialogEnabled = false }
+                }
+            }
+
+            // Delete button
+            // TODO Confirmation prompt
+            if (onDelete != null) {
+                var deleteConfirmationVisible by rememberSaveable { mutableStateOf(false) }
+
+                IconButton(
+                    enabled = selectedIndex != null,
+                    onClick = {
+                        deleteConfirmationVisible = true
+                    }
+                ) {
+                    Icon(
+                        Icons.Default.Delete,
+                        stringResource(R.string.delete),
+                    )
+                }
+
+                if (deleteConfirmationVisible) ConfirmDeleteDialog(
+                    onCloseDialog = { deleteConfirmationVisible = false },
+                    itemStr = itemToName?.invoke(items[selectedIndex!!])
+                ) {
+                    onDelete(selectedIndex!!)
+                    selectedIndex = null
                 }
             }
         }
