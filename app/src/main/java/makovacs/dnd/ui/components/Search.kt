e6068cb7364335deca4644ae4de3269037ba645c
Jordan Kovacs <98484926+JorKov-JAC@users.noreply.github.com>
2023-11-10 23:53:07 -0500

    feat: Implement StringSearchList

--- app/src/main/java/makovacs/dnd/ui/components/Search.kt
+++ app/src/main/java/makovacs/dnd/ui/components/Search.kt
@@ -1,9 +1,23 @@
 package makovacs.dnd.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
@@ -44,4 +58,65 @@ fun <T, Q, R: Comparable<R>> search(
     }
 
     return results.collectAsState().value
+}
+
+/**
+ * A list of items with a search box at the top.
+ *
+ * @param items The items to search through.
+ * @param queryModifier Takes a query string and converts it to a query object that is then passed
+ * to [mapper].
+ * @param mapper Performs both filtering and sorting of items.
+ * It is called with the query object from [queryModifier] and an individual item,
+ * and should return null if the item does not fit the query,
+ * otherwise should return a [Comparable] object which can be used to sort valid items.
+ * @param key Converts an item into a unique key. Necessary if [itemContent] needs to preserve state
+ * when the items are reordered.
+ * @param label Custom label text for the search query text box.
+ * @param itemContent Component displayed for each item. It is called with the item's search result
+ * index and the item itself.
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun <T, Q, R: Comparable<R>> StringSearchList(
+    items: List<T>,
+    queryModifier: (String) -> Q,
+    mapper: (query: Q, item: T) -> R?,
+    key: ((item: T) -> Any)?,
+    modifier: Modifier = Modifier,
+    label: String = "Query",
+    itemContent: @Composable (index: Int, item: T) -> Unit
+) {
+    var query by rememberSaveable { mutableStateOf("") }
+    val searchResults = search(items = items, query = queryModifier(query), mapper = mapper)
+
+    Column(
+        modifier = modifier
+            .fillMaxHeight()
+            .then(modifier)
+    ) {
+        // Query input
+        TextField(
+            query,
+            { query = it },
+            label = { Text(label) },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        if (searchResults == null) {
+            // Show loading text...
+            Text("Searching", modifier = Modifier.align(CenterHorizontally))
+        } else {
+            // Done searching, show results
+            if (searchResults.isEmpty()) {
+                Text("No results.", modifier = Modifier.align(CenterHorizontally))
+            } else LazyColumn(modifier = Modifier) {
+                itemsIndexed(
+                    searchResults,
+                    key = key?.let { { _, item -> key(item) } }
+                ) { index, item -> itemContent(index, item) }
+            }
+        }
+    }
 }
