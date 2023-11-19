fa35937efd38a804e3b2256c92c09e574d766032
Jordan Kovacs <98484926+JorKov-JAC@users.noreply.github.com>
2023-11-19 07:07:54 -0500

    feat: Use EvenWidthGrid throughout Monster UI

--- app/src/main/java/makovacs/dnd/ui/components/monsters/MonsterEditor.kt
+++ app/src/main/java/makovacs/dnd/ui/components/monsters/MonsterEditor.kt
@@ -2,13 +2,11 @@ package makovacs.dnd.ui.components.monsters
 
 import android.graphics.Bitmap
 import android.icu.text.DecimalFormat
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -125,10 +123,7 @@ fun MonsterEditor(
         // Ability scores
         AbilityScoresInput(
             vm.abilityScores,
-            vm.abilityScores::set,
-            modifier = Modifier
-                .fillMaxWidth()
-                .horizontalScroll(rememberScrollState())
+            vm.abilityScores::set
         )
 
         // Challenge Rating
