44e26e6728bd68c45d3c62ff787cbd6803bcbdf7
Jordan Kovacs <98484926+JorKov-JAC@users.noreply.github.com>
2023-12-10 18:33:44 -0500

    fix: Make sign-in/-up screens work in landscape

--- app/src/main/java/makovacs/dnd/ui/screens/SignIn.kt
+++ app/src/main/java/makovacs/dnd/ui/screens/SignIn.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -44,7 +46,7 @@ fun SignIn(authViewModel: AuthViewModel = viewModel(factory = AuthViewModelFacto
     val signInResult by authViewModel.signInResult.collectAsState(ResultAuth.Inactive)
     var message by rememberSaveable { mutableStateOf("") }
 
-    Column {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
         Button(onClick = {
             navController.navigate(Route.SignUp.route)
         }) {
